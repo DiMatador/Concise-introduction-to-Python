b29f1e7e67f541bbdc5486c4a963eb4749ebe6dd
DiMatador <jcdavila1@hotmail.com>
2022-10-06 16:39:47 -0600

    Updated Week1/Assignments/problem1_6, to print out ODD

--- Week1/Assignment1/problem1_6.py
+++ Week1/Assignment1/problem1_6.py
@@ -1,6 +1,7 @@
 #%%
-# print numbers from 1 to 100 that are even
+# print numbers from 1 to 100 that are ODD
 def problem1_6():
     for i in range(1,100,2):
-            print("Even")
+            print("ODD")
             print(i,end=' ')
+problem1_6()
